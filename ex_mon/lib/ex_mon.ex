7100a0fa054da6d436a91e6d9d81bf04bfe42939
Marcos Flavio <mfghost69@gmail.com>
2023-02-18 02:12:49 -0300

    Correcting comments

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,12 +1,14 @@
 defmodule ExMon do
   alias ExMon.{Game, Player} # Creating aliases so I only need to use the laste name to reference
 
+  @computer_name "Robotinik" # Defining a module variable
+
   def create_player(name, move_avg, move_rnd, move_heal) do # Function to create a player
     Player.build(name, move_avg, move_rnd, move_heal) # Creating a player using a defstruct created in ExMon.player
   end
 
   def start_game(player) do # Starting game by first creating a CPU player(computer) and starting the game using game function in ExMon.Game
-    "Robotinik" # Passing the name |>
+    @computer_name # Passing the name |>
     |> create_player(:punch, :kick, :heal) # Passing the computer |>
     |> Game.start(player)
   end
--- ex_mon/lib/player.ex
+++ ex_mon/lib/player.ex
@@ -4,7 +4,7 @@ defmodule ExMon.Player do
   @enforce_keys required_keys # Forces to set all keys(selected) to be able to create the struct
   defstruct required_keys # Defining the struct
 
-  @max_life 100 # Defining a globar var
+  @max_life 100 # Defining a module variable
 
   def build(name, move_avg, move_rnd, move_heal) do # Function to build players. Sets both the user and the computer.
     %ExMon.Player{ # Defing a map to set each value to a key
