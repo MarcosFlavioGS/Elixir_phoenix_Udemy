32b3801b147969d066e93df950f24dff665198e2
Marcos Flavio <mfghost69@gmail.com>
2023-02-18 02:08:09 -0300

    Anotations

--- ex_mon/lib/player.ex
+++ ex_mon/lib/player.ex
@@ -2,12 +2,12 @@ defmodule ExMon.Player do
   required_keys = [:life, :move_avg, :move_heal, :move_rnd, :name]
 
   @enforce_keys required_keys # Forces to set all keys(selected) to be able to create the struct
-  defstruct required_keys
+  defstruct required_keys # Defining the struct
 
-  @max_life 100
+  @max_life 100 # Defining a globar var
 
-  def build(name, move_avg, move_rnd, move_heal) do
-    %ExMon.Player{
+  def build(name, move_avg, move_rnd, move_heal) do # Function to build players. Sets both the user and the computer.
+    %ExMon.Player{ # Defing a map to set each value to a key
       name: name,
       move_rnd: move_rnd,
       move_avg: move_avg,
