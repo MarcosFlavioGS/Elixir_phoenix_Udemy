d4164f7ee2c5ce6b040d3b8430a0415af38b2d36
Marcos Flavio <mfghost69@gmail.com>
2023-03-08 20:40:38 -0300

    Game attacks and gae turns updated

--- ex_mon/lib/game/actions/attack.ex
+++ ex_mon/lib/game/actions/attack.ex
@@ -1,6 +1,6 @@
 defmodule ExMon.Game.Actions.Attack do
   alias ExMon.Game
-
+  alias ExMon.Game.Status
   @move_avg_damage 18..25
   @move_rnd_damage 10..35
 
@@ -11,7 +11,7 @@ defmodule ExMon.Game.Actions.Attack do
     |> Game.fetch_player()
     |> Map.get(:life)
     |> calculate_life(damage)
-    |> update_opponent_life(opponent)
+    |> update_opponent_life(opponent, damage)
   end
 
   defp calculate_damage(:move_avg), do: Enum.random(@move_avg_damage)
@@ -20,16 +20,21 @@ defmodule ExMon.Game.Actions.Attack do
   defp calculate_life(life, damage) when life - damage < 0, do: 0
   defp calculate_life(life, damage), do: life - damage
 
-  defp update_opponent_life(life, opponent) do
+  defp update_opponent_life(life, opponent, damage) do
     opponent
     |> Game.fetch_player()
     |> Map.put(:life, life)
-    |> update_game(opponent)
+    |> update_game(opponent, damage)
   end
 
-  defp update_game(player, opponent) do # Receives the player to update; And the key that will be used
+  # Receives the player to update; And the key that will be used
+  defp update_game(player, opponent, damage) do
     Game.info()
-    |> Map.put(opponent, player) # Update the opponent key with the player's new info
+    # Update the opponent key with the player's new info
+    |> Map.put(opponent, player)
     |> Game.update()
+
+    # Passing opponent, so we can know who did attack
+    Status.print_move_message(opponent, :attack, damage)
   end
 end
--- ex_mon/lib/game/status.ex
+++ ex_mon/lib/game/status.ex
@@ -1,11 +1,30 @@
 defmodule ExMon.Game.Status do
-  alias ExMon.Game
 
-  def print_round_message do
-    IO.puts "\n======The game has started======\n"
-    IO.inspect(Game.info())
-    IO.puts "-----------------------"
+  def print_round_message(%{status: :started} = info) do
+    IO.puts("\n======The game has started======\n")
+    IO.inspect(info)
+    IO.puts("-----------------------")
   end
 
-  def wrong_move_message(move), do: IO.puts "\n======Invalid move: #{move} ======\n"
+  def print_round_message(%{status: :continue, turn: player} = info) do
+    IO.puts("\n======It's #{player}'s turn======\n")
+    IO.inspect(info)
+    IO.puts("-----------------------")
+  end
+
+  def print_round_message(%{status: :game_over} = info) do
+    IO.puts("\n======The game is over======\n")
+    IO.inspect(info)
+    IO.puts("-----------------------")
+  end
+
+  def wrong_move_message(move), do: IO.puts("\n======Invalid move: #{move} ======\n")
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n======The player attacked the computer for #{damage} damage.======\n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n======The computer attacked the player for #{damage} damage.======\n")
+  end
 end
